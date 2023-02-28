e220716efddc501a2c1633c68e5124b865d65a4d
saulLeonar <126531254+saulLeonar@users.noreply.github.com>
2023-02-28 11:14:15 -0600

    Update socioFormadores.py

--- socioFormadores.py
+++ socioFormadores.py
@@ -6,6 +6,7 @@ expenses=["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
 currency="USD"
 page_title = "Incomes and Expense Tracker"
 page_icon=":money_with_wings"
+layout= "centered"
 
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title+""+page_icon)
