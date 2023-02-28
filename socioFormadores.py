7abfbe3ff8c3a75d32c30f59d2b9f013d9955bdd
saulLeonar <126531254+saulLeonar@users.noreply.github.com>
2023-02-28 12:09:18 -0600

    Update socioFormadores.py

--- socioFormadores.py
+++ socioFormadores.py
@@ -20,11 +20,16 @@ st.title(page_title+""+page_icon)
 hora=["0","1","2","3","4","5"]
 
 st.header(f"Registro de {currency}")
+##Descripccion del proyecto##
+with. st expander("Descipción del proyecto: "):
+  description= st.text_area("", placeholder="Ingresa aqui la descripcion del proyecto...")
+ 
+ ##cupo del grupo
 
 ##Horas acreditar##
 with st.form("entry_form", clear_on_submit=True):
   col1,col2=st.columns(2)
   col1.selectbox("Selecciona limite de horas a acreditar:", hora, key="hora")
-
+  
 
 
