c5e8758f0a3611e56e4ffa07be464b0d9a59fe98
saulLeonar <126531254+saulLeonar@users.noreply.github.com>
2023-02-28 11:11:54 -0600

    Update socioFormadores.py

--- socioFormadores.py
+++ socioFormadores.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 
 incomes= ["Salary", "Blog", "Other Incomes"]
 expenses=["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
