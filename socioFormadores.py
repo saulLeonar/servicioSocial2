e9f4eed067b5932ab0c6109bef903f5103da1552
saulLeonar <126531254+saulLeonar@users.noreply.github.com>
2023-02-28 15:17:33 -0600

    Update socioFormadores.py

--- socioFormadores.py
+++ socioFormadores.py
@@ -31,16 +31,6 @@ st.number_input("Horas a acreditar: ", min_value=50, format="%i", step= 1)
 ##Carreras
 carreras=st.multiselect("Seleccione carreras participantes: ", ["Todas","Arquitecto", "Ingeniero Civil","Licenciado en Urbanismo""Licenciado en economía", "Licenciado en Derecho","Licenciado en Relaciones Internacionales", "Licenciado en Gobierno y transformación pública", "Licenciado en Arte digital","Licenciado en Comunicación","Licenciado en Diseño","Licenciado en Innovación Educativa","Licenciado en Letras Hispanicas","Licenciado en periodismo","Licenciado en Tecnología y producción musical","Ingeniero Biomedico", "Ingeniero en Electronica", "Ingeniero en innovacion y desarrollo","Ingeniero industrial y de sistemas","Ingeniero Mecanico","Ingniero Mecatronico"])
 
-##Requisitos
-st.header('Watchlist')
-wt=[]
-container1 = st.beta_container()
-sym = container1.text_input('for adding')
-container2 = st.beta_container()
-add_button = container2.button('add')
-while True:
-  if add_button:
-    wt.extend(sym)
 
 
 #Fechas
