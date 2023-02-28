e790f342c4b75cc91a09ca2ee358d7bf1deee39c
saulLeonar <126531254+saulLeonar@users.noreply.github.com>
2023-02-28 10:49:57 -0600

    Update socioFormadores.py

--- socioFormadores.py
+++ socioFormadores.py
@@ -3,3 +3,9 @@ import plotly.graph_objects as go
 
 incomes= ["Salary", "Blog", "Other Incomes"]
 expenses=["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
+currency="USD"
+page_title = "Incomes and Expense Tracker"
+page_icon=":money_with_wings"
+
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title+""+page_icon)
