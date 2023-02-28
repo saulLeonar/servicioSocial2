008ec09a711dce1e9050a8222e2c3efae340cd2f
saulLeonar <126531254+saulLeonar@users.noreply.github.com>
2023-02-28 14:34:25 -0600

    Update socioFormadores.py

--- socioFormadores.py
+++ socioFormadores.py
@@ -40,7 +40,4 @@ today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
 start_date = st.date_input('Start date', today)
 end_date = st.date_input('End date', tomorrow)
-if start_date < end_date:
-    st.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
-else:
-    st.error('Error: End date must fall after start date.')
+
