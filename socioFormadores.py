76fbd253050527084144c9183c22e8d0047ce4a6
saulLeonar <126531254+saulLeonar@users.noreply.github.com>
2023-02-28 13:02:16 -0600

    Update socioFormadores.py

--- socioFormadores.py
+++ socioFormadores.py
@@ -32,7 +32,7 @@ st.number_input("Horas acreditar: ", min_value=50, format="%i", step= 1)
 #with st.expander("Requisitos para el alumno: "):
 #  requirements= st.text_area("")
 ##Carreras
-carreras=["Todas","Arquitecto", "Ingeniero Civil","Licenciado en Urbanismo","Licenciado en economía", "Licenciado en Derecho","Licenciado en Relaciones Internacionales", "Licenciado en Gobierno y transformacion publica", "Licenciado en Arte digital","Licenciado en Comunicación","Licenciado en Diseño","Licenciado en Innovacion Educativa","Licenciado en Letras Hispanicas","Licenciado en periodismo","Licenciado en Tecnologia y produccion musical","Ingeniero Biomedico", "Ingeniero en Electronica", "Ingeniero en innovacion y desarrollo","Ingeniero industrial y de sistemas","Ingeniero Mecanico","Ingniero Mecatronico"]
+carreras=["Todas","Arquitecto", "Ingeniero Civil","Licenciado en Urbanismo"]
 with st.form("entry_form", clear_on_submit=True):
   col1, col2=st.columns(2)
   col1=selectbox("Seleccione las carreras que pueden participar:", carreras , key="carreras")
