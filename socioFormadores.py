306d68f1dbeb3972a2e2f51cbec151907f5d0d29
saulLeonar <126531254+saulLeonar@users.noreply.github.com>
2023-02-28 14:48:13 -0600

    Update socioFormadores.py

--- socioFormadores.py
+++ socioFormadores.py
@@ -1,6 +1,7 @@
 
 import datetime
 import streamlit as st
+import SessionState
 #import plotly.graph_objects as go
 
 
@@ -37,17 +38,19 @@ carreras=st.multiselect("Seleccione carreras participantes: ", ["Todas","Arquite
 
 ##Requisitos
 wt = ['initialised text']
-with watchlist:
-     st.header('Watchlist')
-     #create 2 columns inside watchlist header
-     col1 = st.beta_columns()
-     with col1:
-         container1 = st.beta_container()
-         sym = container1.text_input('for adding')
-         add_button = container2.button('add')
-         if add_button:
-             wt.append(sym)
- st.write(wt)
+ss = SessionState.get(wt=wt)
+
+st.header('Watchlist')
+
+container1 = st.beta_container()
+sym = container1.text_input('for adding')
+container2 = st.beta_container()
+add_button = container2.button('add')
+
+if add_button:
+    ss.wt.append(sym)
+
+st.write(ss.wt)
 
 
 #Fechas
