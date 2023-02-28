986d199bc98a303476420995106d66468ee63d28
saulLeonar <126531254+saulLeonar@users.noreply.github.com>
2023-02-28 11:33:57 -0600

    Update socioFormadores.py

--- socioFormadores.py
+++ socioFormadores.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 #incomes= ["Salary", "Blog", "Other Incomes"]
 #expenses=["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
-#currency="USD"
+currency="USD"
 page_title = "Socio formadores"
 page_icon=":office_worker:"
 layout= "centered"
@@ -18,8 +18,11 @@ st.title(page_title+""+page_icon)
 years = [datetime.today().year, datetime.today().year+1]
 months= list(calendar.month_name[1:])
 
-
-#st.header(f"Data Entryin {currency}")
+st.header(f"Data Entry in {currency}")
+with st.form("entry_form", clear_on_submit=True):
+  col1,col2=st.columns(2)
+  col1.selectbox("Select Month:",months, key="month")
+  col2.selectbox("Select year:", years,key="year")
 
 
 
