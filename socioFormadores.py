c13287c9aa350832df26499557f5d95503b6e55d
saulLeonar <126531254+saulLeonar@users.noreply.github.com>
2023-02-28 11:49:21 -0600

    Update socioFormadores.py

--- socioFormadores.py
+++ socioFormadores.py
@@ -1,5 +1,5 @@
-import calendar
-from datetime import datetime 
+#import calendar
+#from datetime import datetime 
 
 import streamlit as st
 #import plotly.graph_objects as go
@@ -7,7 +7,7 @@ import streamlit as st
 
 #incomes= ["Salary", "Blog", "Other Incomes"]
 #expenses=["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
-currency="USD"
+currency="Proyectos"
 page_title = "Socio formadores"
 page_icon=":office_worker:"
 layout= "centered"
@@ -15,14 +15,18 @@ layout= "centered"
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title+""+page_icon)
 
-years = [datetime.today().year, datetime.today().year+1]
-months= list(calendar.month_name[1:])
 
-st.header(f"Data Entry in {currency}")
+
+hora=[]
+for i in range (0:100):
+  horas.append(i)
+
+st.header(f"Registro de {currency}")
+
+##Horas acreditar##
 with st.form("entry_form", clear_on_submit=True):
-  col1,col2=st.columns(2)
-  col1.selectbox("Select Month:",months, key="month")
-  col2.selectbox("Select year:", years,key="year")
+  col1=st.columns(1)
+  col1.selectbox("Selecciona limite de horas a acreditar:", hora, key="hora")
 
 
 
