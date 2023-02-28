add7ac0f443a8ad3fc01caf44a0ee2a40782b517
saulLeonar <126531254+saulLeonar@users.noreply.github.com>
2023-02-28 14:21:18 -0600

    Update socioFormadores.py

--- socioFormadores.py
+++ socioFormadores.py
@@ -18,19 +18,20 @@ st.header(f"Registro de {currency}")
 with st.expander("Nombre del proyecto: "):
   name= st.text_area("",)
  
-##Descripccion del proyecto##
+##Descripción del proyecto##
 with st.expander("Descipción del proyecto: "):
   description= st.text_area("", placeholder="Ingresa aqui la descripcion del proyecto...")
  
-##cupo del grupo
-st.number_input("Numero de alumnos que se necsita: ", min_value=0, format="%i", step= 1)
+##Cupo del grupo
+st.number_input("Número de alumnos que se necesitan: ", min_value=0, format="%i", step= 1)
 
-##Horas acreditar##
-st.number_input("Horas acreditar: ", min_value=50, format="%i", step= 1)
+##Horas a acreditar##
+st.number_input("Horas a acreditar: ", min_value=50, format="%i", step= 1)
 
 ##FRequisitos
 #with st.expander("Requisitos para el alumno: "):
 #  requirements= st.text_area("")
 ##Carreras
-carreras=["Todas","Arquitecto", "Ingeniero Civil","Licenciado en Urbanismo""Licenciado en economía", "Licenciado en Derecho","Licenciado en Relaciones Internacionales", "Licenciado en Gobierno y transformacion publica", "Licenciado en Arte digital","Licenciado en Comunicación","Licenciado en Diseño","Licenciado en Innovacion Educativa","Licenciado en Letras Hispanicas","Licenciado en periodismo","Licenciado en Tecnologia y produccion musical","Ingeniero Biomedico", "Ingeniero en Electronica", "Ingeniero en innovacion y desarrollo","Ingeniero industrial y de sistemas","Ingeniero Mecanico","Ingniero Mecatronico"]
-st.selectbox("Seleccione las carreras que pueden participar:", carreras , key="carreras")
+carreras=st.multiselct("Carreras:" ["Todas","Arquitecto", "Ingeniero Civil","Licenciado en Urbanismo""Licenciado en economía", "Licenciado en Derecho","Licenciado en Relaciones Internacionales", "Licenciado en Gobierno y transformación pública", "Licenciado en Arte digital","Licenciado en Comunicación","Licenciado en Diseño","Licenciado en Innovación Educativa","Licenciado en Letras Hispanicas","Licenciado en periodismo","Licenciado en Tecnología y producción musical","Ingeniero Biomedico", "Ingeniero en Electronica", "Ingeniero en innovacion y desarrollo","Ingeniero industrial y de sistemas","Ingeniero Mecanico","Ingniero Mecatronico"])
+st.write(carreras)
+ 
