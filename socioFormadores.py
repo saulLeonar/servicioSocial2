b562971f4d69c6bba9192c8db04005c2bd3e0b83
saulLeonar <126531254+saulLeonar@users.noreply.github.com>
2023-02-28 12:51:56 -0600

    Update socioFormadores.py

--- socioFormadores.py
+++ socioFormadores.py
@@ -29,8 +29,8 @@ st.number_input("Numero de alumnos que se necsita: ", min_value=0, format="%i", 
 st.number_input("Horas acreditar: ", min_value=50, format="%i", step= 1)
 
 ##FRequisitos
-with st.expander("Requisitos para el alumno: "):
-  requirements= st.text_area("")
+#with st.expander("Requisitos para el alumno: "):
+#  requirements= st.text_area("")
 ##Carreras
 carreras=["Todas","Arquitecto", "Ingeniero Civil","Licenciado en Urbanismo","Licenciado en economía", "Licenciado en Derecho","Licenciado en Relaciones Internacionales", "Licenciado en Gobierno y transformacion publica", "Licenciado en Arte digital","Licenciado en Comunicación","Licenciado en Diseño","Licenciado en Innovacion Educativa","Licenciado en Letras Hispanicas","Licenciado en periodismo","Licenciado en Tecnologia y produccion musical","Ingeniero Biomedico", "Ingeniero en Electronica", "Ingeniero en innovacion y desarrollo","Ingeniero industrial y de sistemas","Ingeniero Mecanico","Ingniero Mecatronico"]
 with st.form(entry_form, clear_on_submit=True):
