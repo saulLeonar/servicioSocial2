0e0c2c5fb8b7a1c0b0817abfbd271508ed73998b
saulLeonar <126531254+saulLeonar@users.noreply.github.com>
2023-02-28 15:49:09 -0600

    Update socioFormadores.py

--- socioFormadores.py
+++ socioFormadores.py
@@ -31,12 +31,14 @@ st.number_input("Horas a acreditar: ", min_value=50, format="%i", step= 1)
 carreras=st.multiselect("Seleccione carreras participantes: ", ["Todas","Arquitecto", "Ingeniero Civil","Licenciado en Urbanismo""Licenciado en economía", "Licenciado en Derecho","Licenciado en Relaciones Internacionales", "Licenciado en Gobierno y transformación pública", "Licenciado en Arte digital","Licenciado en Comunicación","Licenciado en Diseño","Licenciado en Innovación Educativa","Licenciado en Letras Hispanicas","Licenciado en periodismo","Licenciado en Tecnología y producción musical","Ingeniero Biomedico", "Ingeniero en Electronica", "Ingeniero en innovacion y desarrollo","Ingeniero industrial y de sistemas","Ingeniero Mecanico","Ingniero Mecatronico"])
 
 ## Requisitos
-x=["Añadir requisitos"]
-container2 = st.beta_container()
-add_button = container2.button('add')
-requisitos=st.multiselect("Requerimientos: ", x)
-if add_button:
-    x.append(requisitos)
+st.title("Requisitos")
+requirements = st.text_area("Ingrese los requisitos, uno por línea")
+requirements = requirements.split("\n")
+requirements = list(filter(None, requirements))
+if len(requirements) > 0:
+  st.write("Requisitos:")
+  for i, requirement in enumerate(requirements):
+    st.write(f"{i+1}. {requirement}")
 
 #Fechas
 today = datetime.date.today()
